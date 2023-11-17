b9c74973abaa07c0eeba07fa7b7b49c7bd9fda62
Nicolás Agustín Enrico <91215@sistemas.frc.utn.edu.ar>
2023-11-17 10:54:38 -0300

    cors

---
+++ src/main/java/k7/grupo7/ppai/config/CORSConfig.java
@@ -0,0 +1,20 @@
+package k7.grupo7.ppai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
