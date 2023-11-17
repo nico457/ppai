9ca8417628b406c96e195504f7680f6c35746956
Nicolás Agustín Enrico <91215@sistemas.frc.utn.edu.ar>
2023-11-17 14:16:49 -0300

    port

--- src/main/java/k7/grupo7/ppai/config/CORSConfig.java
+++ src/main/java/k7/grupo7/ppai/config/CORSConfig.java
@@ -12,7 +12,7 @@ public class CORSConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3001")
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
