c949f9384e5316fdac6a5e7258e699ebc95f42da
Nicolás Agustín Enrico <91215@sistemas.frc.utn.edu.ar>
2023-11-17 11:08:28 -0300

    csv

--- src/main/java/k7/grupo7/ppai/application/controllers/LlamadaController.java
+++ src/main/java/k7/grupo7/ppai/application/controllers/LlamadaController.java
@@ -1,9 +1,15 @@
 package k7.grupo7.ppai.application.controllers;
 
+import jakarta.annotation.Resource;
+
 import k7.grupo7.ppai.application.response.LlamadaDetalleResponse;
 import k7.grupo7.ppai.application.response.LlamadaResponse;
 import k7.grupo7.ppai.entities.Llamada;
 import k7.grupo7.ppai.repository.LlamadaRepository;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -31,8 +41,10 @@ public class LlamadaController {
         Llamada llamada = llamadaRepository.findById(id).orElseThrow();
         return ResponseEntity.ok(LlamadaDetalleResponse.from(llamada));
     }
+
+
     @GetMapping("/{id}/csv")
-    public ResponseEntity generarCsv(@PathVariable Integer id){
+    public ResponseEntity<byte[]> generarCsv(@PathVariable Integer id) {
         Llamada llamada = llamadaRepository.findById(id).orElseThrow();
 
         try {
@@ -41,19 +53,30 @@ public class LlamadaController {
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             PrintWriter printWriter = new PrintWriter(bufferedWriter);
 
-
-           printWriter.println("ID: " + llamada.getId());
-
-
+            printWriter.println("ID: " + llamada.getId());
 
             printWriter.flush();
             printWriter.close();
 
-            return ResponseEntity.ok().build();
+            // Leer contenido del archivo en un byte array
+            Path path = Paths.get(archivo);
+            byte[] contenido = Files.readAllBytes(path);
+
+            // Configuración de la respuesta para abrir el archivo automáticamente
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=archivo.csv");
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(contenido.length)
+                    .body(contenido);
 
         } catch (IOException e) {
             return ResponseEntity.badRequest().build();
         }
     }
+
+
 
 }
